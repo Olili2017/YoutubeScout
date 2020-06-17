2b8bb442a5657638c8c1618669ceed0caf1475ac
Olili2017 <olilidaniel48@gmail.com>
2020-06-17 16:45:06 +0300

    clean up unnecesary files

--- server.py
+++ server.py
@@ -10,9 +10,10 @@ def home():
 
 @app.route("/scout/youtube/today")
 def fetchTodayVideos():
-  scout = Scout()
+  scout = Scout(channel_name="good morning britain", key_word="rashford", all=True)
   # print([{"title" : video.getTitle(), "url" : video.getUrl(), "thumbnail": video.getThumbnail()} for video in scout.findVideos()])
   return {"message": "200 OK", "data" : [{"title" : video.getTitle(), "url" : video.getUrl(), "thumbnail": video.getThumbnail()} for video in scout.findVideos()] }
 
 if __name__ == '__main__':
   app.run(port=3000, host='0.0.0.0')
+
--- youtube/video/video.py
+++ youtube/video/video.py
@@ -2,12 +2,58 @@ from pytube import YouTube, Stream
 
 class YouTubeVideo:
 
-  def __init__(self, youtube_link, **args):
-    self.__video = YouTube(youtube_link)
-    self.__url = self.__video.streams.first().url
-    self.__title = self.__video.title
-    self.__thumbnail = self.__video.thumbnail_url
-    self.__res = [stream for stream in self.__video.streams if stream.resolution == ('360p' if args.get('res', None) == None else args.get('res', None))]
+  def __init__(self, youtube_link):
+    self.__setUrl(youtube_link)
+    # self.__video = YouTube(youtube_link)
+    # self.__url = self.__video.streams.first().url
+    # self.__title = self.__video.title
+    # self.__thumbnail = self.__video.thumbnail_url
+    # self.__res = [stream for stream in self.__video.streams if stream.resolution == ('360p' if args.get('res', None) == None else args.get('res', None))]
+
+  # def __init__(self, name, url, thumbnail_url):
+  #   self.__setTitle(name)
+  #   self.__setUrl(url)
+  #   self.__setThumbnail(thumbnail_url)
+
+  def getDownloadUrl(self, *urls, **props):
+    url = ""
+    # check if url given as string argument
+    if len(urls) is not 0:
+      url = urls[0]
+      return self.__scoutDownloadUrl(url)
+
+    # check if key word params are given
+    if len(props) is not 0:
+      # check if video ID is given
+      if props.get("id", None) is not None:
+        url = f'https://youtube.com/watch?v={props.get("id", None)}'
+        return self.__scoutDownloadUrl(url)
+
+      # check if one time video url is given
+      if props.get("url", None) is not None:
+        url = props.get("url", None)
+        return self.__scoutDownloadUrl(url)
+
+    # use object centric url
+    url = self.getUrl()
+
+    self.__scoutDownloadUrl(url)
+    # url = f'https://youtube.com/watch?v={args.get("id", None)}' if args.get("id", None) is not None else self.getUrl()
+    # print(args)
+    # self.__setUrl(args.get("id", self.getUrl()))
+    # self.__video = YouTube(youtube_link)
+    # self.__url = self.__video.streams.first().url
+    # self.__title = self.__video.title
+    # self.__thumbnail = self.__video.thumbnail_url
+    # self.__res = [stream for stream in self.__video.streams if stream.resolution == ('360p' if args.get('res', None) == None else args.get('res', None))]
+
+  # def getDownloadUrl(self, *args):
+  #   print("passing with no namems")
+    # return self.getDownloadUrl(url=url)
+
+  def __scoutDownloadUrl(self, url:str):
+    yt = YouTube(url)
+    return yt.streams.first().url
 
   def __setUrl(self, url):
     self.__url = url
@@ -68,3 +114,7 @@ class ScrapedVideo:
 
   def getThumbnail(self):
     return self.__thumbnail
+
+
+v = YouTubeVideo("https://www.youtube.com/watch?v=AMcWwZumRkQ")
+print(v.getDownloadUrl())
